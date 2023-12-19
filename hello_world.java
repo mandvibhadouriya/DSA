2c3f65224441e57e41a8b6df96892a775acbc4bc
manuthakur0909 <thakur.manu0909@gmail.com>
2023-12-19 21:14:37 +0530

    Hello world Java

---
+++ hello_world.java
@@ -0,0 +1,9 @@
+
+public class hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I'm Mandvi");
+        System.out.println("I'm here to learn Java");
+    }
+}
