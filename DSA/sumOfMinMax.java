71ee1223dfb535615a5cd54a7f5b62c20b3940db
manuthakur0909 <thakur.manu0909@gmail.com>
2023-12-21 23:41:32 +0530

    LeetCode and GFG Solution With Problems

--- DSA/sumOfMinMax.java
+++ DSA/sumOfMinMax.java
@@ -1,3 +1,24 @@
+/*
+Given an array A of size N of integers. Your task is to find the sum of minimum and maximum element in the array.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {-2, 1, -4, 5, 3}
+Output: 1
+Explanation: min = -4, max =  5. Sum = -4 + 5 = 1
+ 
+
+Example 2:
+
+Input:
+N = 4
+A[]  = {1, 3, 4, 1}
+Output: 5
+Explanation: min = 1, max = 4. Sum = 1 + 4 = 5
+ */
+
 class Solution
 { 
     public static int findSum(int A[],int N) 
