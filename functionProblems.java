5f5f3082c140690235407c7750fb581b2503c76c
manuthakur0909 <thakur.manu0909@gmail.com>
2023-12-22 17:47:44 +0530

    Function Problems in Java

---
+++ functionProblems.java
@@ -0,0 +1,159 @@
+import java.util.*;
+public class functionProblems{
+
+    // public static void printMyName(String name){
+    //     System.out.println(name);
+    //     return;
+    // }
+
+    // public static int calculateSum(int a, int b){
+    //     int sum = a + b;
+    //     return sum; 
+    // }
+
+    // public static int calculateProduct(int a, int b){
+    //     int product = a * b;
+    //     return product; 
+    // }
+
+    // public static void printFactorial(int n){
+    //     if(n<0){
+    //         System.out.println("Invalid Number");
+    //         return;
+    //     }
+    //     int factorial = 1;
+
+    //     for(int i=n; i>=1;i--){
+    //         factorial = factorial * i;
+    //     }
+    //     System.out.println(factorial);
+    //     return;
+    // }
+
+    // Make a function to check if a number is prime or not.
+    // public static void checkPrime(int num){
+    //     if(num<=1){
+    //     System.out.println("InValid Number");
+    //     return;
+    //     }
+    //     int temp = 0;
+    //     for(int i=2;i<=num-1;i++){
+    //         if(num%i==0){
+    //             temp = temp+1;
+    //         }
+    //     }
+    //     if(temp>1){
+    //         System.out.println("Non-Prime Number");
+    //     }else{
+    //     System.out.println("Prime Number");
+    //     }
+    // }
+
+    // Make a function to check if a given number n is even or not.
+    // public static void checkEvenOrOdd(int num){
+    //     if (num%2==0){
+    //         System.out.println("Even Number");
+    //         return;
+    //     }else{
+    //         System.out.println("Odd Number");
+    //         return;
+    //     }
+    // }
+
+    // Make a function to print the table of a given number n.
+    // public static void printTable(int num){
+    //     for (int i=1;i<=10;i++){
+    //         System.out.println(num + " x " + i + " = " + num*i);
+    //     }
+    // }
+
+    // Enter 3 numbers from the user & make a function to print their average.
+    // public static void calculateAverage(float num1, float num2, float num3){
+    //     float avg = (num1 + num2 + num3)/3;
+    //     System.out.println(avg);
+    // }
+
+    // Write a function to print the sum of all odd numbers from 1 to n.
+    // public static void printSumOfAllOdd(int n){
+    //     int temp = 1;
+    //     for (int i=2;i<=n;i++){
+    //         if(i%2!=0){
+    //             temp = temp + i;
+    //         }
+    //     }
+    //     System.out.println(temp);
+    // }
+
+    // Write a function which takes in 2 numbers and returns the greater of those two.
+    // public static void printGreaterNum(int a,int b){
+    //     if(a>b){
+    //         System.out.println(a + " Is Greater");
+    //         return;
+    //     }else{
+    //         System.out.println(b + " Is Greater");
+    //         return;
+    //     }
+    // }
+
+    // Write a function that takes in the radius as input and returns the circumference of a circle.
+    // public static void calculateCircumferenceOfCircle(double r){
+    //     double circumference = 2*Math.PI*r;
+    //     System.out.println("Circumference of Circle is : " + circumference);
+    // }
+
+    // Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+        // public static void printEligibilityToVote(int age){
+        //     if(age >= 18){
+        //         System.out.println("Yes! Eligible to Vote");
+        //         return;
+        //     }else{
+        //         System.out.println("No! not eligible to Vote");
+        //         return;
+        //     }
+        // }
+
+        // Write an infinite loop using do while condition.
+        // public static void infiniteLoop(){
+        //     do{
+        //         System.out.println("This is an Infinite Loop using Do While. Press Ctrl+C to exit..");
+        //     }while(true);
+        // }
+
+        // Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered. 
+        // Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. xn.
+        // Write a function that calculates the Greatest Common Divisor of 2 numbers.
+        // Write a program to print Fibonacci series of n terms where n is input by user :
+        // 0 1 1 2 3 5 8 13 21 ..... 
+        // In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.
+
+
+    public static void  main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int age = sc.nextInt();
+        // double r = sc.nextDouble();
+        // int n = sc.nextInt();
+        // String name = sc.next();
+        // int num = sc.nextInt();
+        // float num1 = sc.nextFloat();
+        // float num2 = sc.nextFloat();
+        // float num3 = sc.nextFloat();
+        // int sum = calculateSum(a,b);
+        // int product = calculateProduct(a,b);
+        // printFactorial(n);
+        // printMyName(name);
+        // checkPrime(num);
+        // checkEvenOrOdd(num);
+        // printTable(num);
+        // System.out.println("Sum of a and b is : "+sum);
+        // System.out.println("Product of a and b is : "+product);
+        // calculateAverage(num1,num2,num3);
+        // printSumOfAllOdd(n);
+        // printGreaterNum(a,b);
+        // calculateCircumferenceOfCircle(r);
+        // printEligibilityToVote(age);
+        // infiniteLoop();
+        System.out.println("Hello!! World..");
+    }
+}
