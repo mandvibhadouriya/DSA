dbb01a0f0dfdab17bea7c9269105ee2a2a5acc48
manuthakur0909 <thakur.manu0909@gmail.com>
2024-01-29 11:37:19 +0530

    Learn Recursion

--- recursion.java
+++ recursion.java
@@ -18,15 +18,84 @@ public class recursion{
     }
     // pattern method
     public static void printPattern(int n){
-        for(int i = 1;i<n;i++){
-            for(int j = 1;j<=i;j++){
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("\nOther pattern\n");
+        // Another pattern
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n-i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+
+    // print pattern using recursion
+    public static void patternPrint(int n){
+        if(n>0){
+            patternPrint(n-1);
+            for(int i = 0;i<n;i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //another pattern
+        // if(n>0){
+        //     for(int i=0;i<n;i++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //     patternPrint(n-1);
+        // }
+    }
+    // Celcius temperature into Farenheit
+    public static float celciusToFarenheit(int celcius){
+        return (float)celcius * 9/5 + 32;
+    }
     // recursive function to print the sum of first n natural numbers
+    public static void sumOfNaturalNums(int n,int i,int sum){
+        if(i>n){
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        sumOfNaturalNums(n,i+1,sum);
+    }
     
+    //nth term of Fibonacci series
+    public static int nthTermFibonacci(int privpriv, int priv, int n){
+        if(n<=1){
+            return privpriv;
+        }
+        return nthTermFibonacci(priv,privpriv + priv,n-1);
+    }
+    //using for loop
+    public static int nthTermFibonacci(int n){
+        int privpriv = 0;
+        int priv = 1;
+        int result = 0;
+        for(int i = 2; i<n;i++){
+            result = privpriv + priv;
+            privpriv = priv;
+            priv = result;
+        }
+        return result;
+    }
+    // print avg of the numbers
+    public static void printAverage(int ... arr){
+        int sum = 0;
+        for(int i : arr){
+            sum += i;
+        }
+        float avg = (float)sum/arr.length;
+        System.out.println(avg);
+    }
+
+
     public static void main(String args[]){
 
         //factorial of a number
@@ -34,8 +103,13 @@ public class recursion{
         // System.out.println(factorial(n));
         // multiplicationTable(4,1);
         // printPattern(5);
-        
-
+        // sumOfNaturalNums(5,1,0);
+        // int N = nthTermFibonacci(5);
+        // System.out.println(N);
+        // printAverage(1,2,3,4,5,6,7,8,9);
+        // patternPrint(5);
+        float Farenheit = celciusToFarenheit(22);
+        System.out.println(Farenheit);
 
 
         // int n = 5;
